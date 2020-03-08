6bd966dbc268c44ab30afdda55bd02edc1ebe539
Chelseasc <sshencchen@sina.com>
2020-03-08 17:00:05 +0800

    e

---
+++ pages/info/info.vue
@@ -0,0 +1,52 @@
+<template>
+	<view class="content">
+		<view class="title">{{title}}</view>
+		<view class="art-content">
+			<rich-text :nodes="content"></rich-text>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '',
+				content: ''
+			}
+		},
+		onLoad(e) {
+			console.log('onload:',e)
+			uni.showLoading({
+				title: '加载中。。。'
+			})
+			uni.request({
+				url: `https://unidemo.dcloud.net.cn/api/news/36kr/${e.newsid}`,
+				method: 'GET',
+				data: {},
+				success: res => {
+					console.log(res);
+					this.title = res.data.title;
+					this.content = res.data.content;
+					uni.hideLoading();
+				},
+				fail: () => {},
+				complete: () => {}
+			});
+		},
+		methods: {
+		}
+	}
+</script>
+
+<style>
+	.content {
+		padding: 10upx 2%;
+		width: 100%;
+		flex-wrap: wrap;
+	}
+	.title {line-height: 2em; font-size: 38upx;}
+	.art-content {
+		line-height: 2em; font-size: 24upx;
+	}
+</style>
